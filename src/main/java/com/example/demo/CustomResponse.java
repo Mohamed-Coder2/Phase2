69cd342b70e3c9a554ae7b249d5b778058437119
Mohamed <mohamedemad8862@gmail.com>
2023-12-30 22:02:03 +0200

    Made CustomResponse A Record

--- src/main/java/com/example/demo/CustomResponse.java
+++ src/main/java/com/example/demo/CustomResponse.java
@@ -6,20 +6,5 @@ import java.util.List;
 import java.util.Map;
 
 //helper class for the ProductController GetMapping so it's organized as the way you see it
-public class CustomResponse {
-    private List<Product> products;
-    private Map<String, Integer> categories;
-
-    public CustomResponse(List<Product> products, Map<String, Integer> categories) {
-        this.products = products;
-        this.categories = categories;
-    }
-
-    public List<Product> getProducts() {
-        return products;
-    }
-
-    public Map<String, Integer> getCategories() {
-        return categories;
-    }
+public record CustomResponse(List<Product> products, Map<String, Integer> categories) {
 }
